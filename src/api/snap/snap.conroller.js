f526896f10e88550e9770e5bca80acf7ac770cfa
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 16:04:06 -0600

    feat: Add redirection handling for root URL

--- src/api/snap/snap.conroller.js
+++ src/api/snap/snap.conroller.js
@@ -3,23 +3,26 @@ import { domain, isValidURL, addHttpsIfNeeded } from '../../utils/utils.js';
 import { screenshot } from '../../utils/screenshot.util.js';
 
 export async function postSnap(req, res) {
-	const { url, size, quality } = req.query;
+	if (req.originalUrl === '/') {
+		const { url, size, quality } = req.query;
+		if (url === undefined) {
+			throw new ValidationError(`Please call via ${domain}/?url=<domain>`);
+		}
 
-	if (url === undefined) {
-		throw new ValidationError(`Please call via ${domain}/?url=<domain>`);
+		if (!isValidURL(url)) {
+			throw new ValidationError('invalid url');
+		}
+
+		const captured = await screenshot({
+			url: addHttpsIfNeeded(url),
+			quality,
+			size,
+		});
+
+		return res.status(307).redirect(captured.path);
 	}
 
-	if (!isValidURL(url)) {
-		throw new ValidationError('invalid url');
-	}
-
-	const captured = await screenshot({
-		url: addHttpsIfNeeded(url),
-		quality,
-		size,
-	});
-
-	return res.status(307).redirect(captured.path);
+	throw new UnimplementedFunctionError();
 
 	// return res.status(200).json({
 	// 	message: 'ok',
