fa9d0c275f1693e52691a106a60eac86497fe737
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 14:53:46 -0600

    refactor: Refactor error classes to use functions instead of classes

--- src/app.error.js
+++ src/app.error.js
@@ -1,30 +1,21 @@
-class HttpError extends Error {
-	constructor(statusCode, message) {
-		super(message);
-		this.statusCode = statusCode;
-	}
+function HttpError(statusCode, message) {
+	const error = new Error(message);
+	error.statusCode = statusCode;
+	return error;
 }
 
-export class ForbiddenError extends HttpError {
-	constructor(message = 'Forbidden') {
-		super(403, message);
-	}
+export function ForbiddenError(message = 'Forbidden') {
+	return HttpError(403, message);
 }
 
-export class UnauthorizedError extends HttpError {
-	constructor(message = 'Unauthorized') {
-		super(401, message);
-	}
+export function UnauthorizedError(message = 'Unauthorized') {
+	return HttpError(401, message);
 }
 
-export class NotFoundError extends HttpError {
-	constructor(message = 'Not Found') {
-		super(404, message);
-	}
+export function NotFoundError(message = 'Not Found') {
+	return HttpError(404, message);
 }
 
-export class ValidationError extends HttpError {
-	constructor(message = 'Validation Error') {
-		super(422, message);
-	}
+export function ValidationError(message = 'Validation Error') {
+	return HttpError(422, message);
 }
