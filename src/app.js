8ef6f1f357d49510a5d06458e069f28532cf97e3
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 12:30:24 -0600

    fix: Correct typo in 404 error message

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.get('/healthz', (req, res) => res.status(200).json({ message: 'ok' }));
 
-app.use((req, res, next) => res.status(404).json({ message: 'nod found!' }));
+app.use((req, res, next) => res.status(404).json({ message: 'not found!' }));
 app.use((err, req, res, next) => res.status(500).json({ message: 'error!' }));
 
 export { app };
