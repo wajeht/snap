510ccd7660c3f518f64d9b7f74973e7a712290ca
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 12:36:32 -0600

    feat: Add cors, helmet, and compression middleware in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
-import { api } from './api/api.js'
 import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
+
+import { api } from './api/api.js';
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
