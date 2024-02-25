53c35ff1f1f1dee9a8168c50015ab944fbaf4a72
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 11:36:33 -0600

    feat: Add initial project files and configurations

---
+++ src/config/s3.config.js
@@ -0,0 +1,9 @@
+export const s3 = {
+  region: 'eu-east-1',
+  credentials: {
+    accessKeyId: '5wQKcegykSIdpz9gW1aT',
+    secretAccessKey: 'fPi3ovFlXt8QexlrEgp3QfREGu75awQgtReknRa8'
+  },
+  endpoint: 'https://minio-api.jaw.dev',
+  forcePathStyle: true
+};
