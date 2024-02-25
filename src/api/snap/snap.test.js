f42910041e6e6b492dd22d217da4bd3a23a75b8b
wajeht <58354193+wajeht@users.noreply.github.com>
2024-02-25 16:13:01 -0600

    test: Add unit test for calling / route

---
+++ src/api/snap/snap.test.js
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import { it, expect } from 'vitest';
+import { app as server } from '../../app.js';
+
+const app = request(server);
+
+it('should be able to call /', async () => {
+	const response = await app.get('/');
+	expect(response.status).toBe(422);
+	expect(response.body.message).include('Error: Please call via');
+});
